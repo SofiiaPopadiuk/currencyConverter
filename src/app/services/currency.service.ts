cdf932bf6be9c86725db7dac2e93c96bcd3e46bc
Sofiia Popadiuk <popadiuk.sophiya@gmail.com>
2024-09-09 22:59:39 +0300

    add handling of storing environment variables

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -7,8 +7,6 @@ import { BaseCurrencyRate } from '../models/currency.model';
 import { setCurrencyRates } from '../store/actions/currencies.actions';
 import { selectCurrencyRate } from '../store/selectors/currencies.selectors';
 
-const API_KEY = '3291bb259083ca4f298b6ac3';
-
 @Injectable({
   providedIn: 'root',
 })
@@ -35,7 +33,7 @@ export class CurrencyService {
   ): Observable<{ [currencyCode: string]: number }> {
     return this.http
       .get<BaseCurrencyRate>(
-        `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${baseCurrency}`,
+        `https://v6.exchangerate-api.com/v6/${process.env['API_KEY']}/latest/${baseCurrency}`,
       )
       .pipe(
         tap((rates) => {
